fda6cb1b26e994f5828930cf29bea9857ac29591
Lian jianheng <583761743@qq.com>
2022-04-18 14:08:52 +0800

    some addition on 4.18

---
+++ src/main/java/edu/nju/StudentServiceApplication.java
@@ -0,0 +1,15 @@
+package edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StudentServiceApplication.class, args);
+    }
+
+}
